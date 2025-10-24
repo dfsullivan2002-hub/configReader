3190187d9387aa5aa29ce9a7ea54e86da3c0a0f9
Danny <danny@Daniels-MacBook-Pro-3.local>
2025-10-23 22:17:52 -0400

    Add config reader files

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 //Input config file and this script will read it
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
 
 int main () {
 
     std::ifstream inFile;
+    std::map <std::string, std::string> settings;
 
     inFile.open("settings.ini");
 
     if (inFile.is_open()) {
         std::string line;
         while(std::getline(inFile, line)) {
-            std::cout << line << std::endl;
+            if (line.empty() || line[0] == '#') {
+                continue;
+            } else {
+
+            int idx = line.find('=');
+            std::string key = line.substr(0, idx);
+            std::string value = line.substr(idx + 1);
+            settings[key] = value;
+            std::cout << key << value << std::endl;
+
+            }
         }
         inFile.close();
     } 
     else {
         std::cout << "ERROR! File Not Found." << std::endl;
     }
-
     return 0;
 }
